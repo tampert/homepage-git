24a4731882f78113854655efeddf1ffefe209396
Dirk Soentjens <dirksoentjens@Dirks-MacBook-Pro.local>
2025-07-31 12:36:51 +0200

    Add Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <v-container>
+    <v-row no-gutters>
+      <v-col cols="12">
+        <h1>https://tampert.github.io</h1>
+        <a
+          href="https://tampert.github.io/resume-react/"
+          target="_blank"
+          rel="noopener"
+          >resume-react</a
+        >
+        to https://tampert.github.io/resume-react/<br />
+        <a
+          href="https://tampert.github.io/resume-angular/"
+          target="_blank"
+          rel="noopener"
+          >resume-angular</a
+        >
+        to https://tampert.github.io/resume-angular/<br />
+        <a
+          href="https://tampert.github.io/resume-vue/"
+          target="_blank"
+          rel="noopener"
+          >resume-vue</a
+        >
+        to https://tampert.github.io/resume-vue/
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped></style>
