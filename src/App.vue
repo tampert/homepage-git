556c875a1f7e7d2dd5a3e4117159460471fadfc5
Dirk Soentjens <dirksoentjens@Dirks-MacBook-Pro.local>
2025-07-31 14:19:19 +0200

    Add list

--- src/App.vue
+++ src/App.vue
@@ -4,28 +4,47 @@
   <v-container>
     <v-row no-gutters>
       <v-col cols="12">
-        <h1>https://tampert.github.io</h1>
-        <a
-          href="https://tampert.github.io/resume-react/"
-          target="_blank"
-          rel="noopener"
-          >resume-react</a
+        <v-card
+          class="mx-auto"
+          max-width="600"
+          rounded="lg"
+          title="Routes"
+          subtitle="Routes to my different github pages"
         >
-        to https://tampert.github.io/resume-react/<br />
-        <a
-          href="https://tampert.github.io/resume-angular/"
-          target="_blank"
-          rel="noopener"
-          >resume-angular</a
-        >
-        to https://tampert.github.io/resume-angular/<br />
-        <a
-          href="https://tampert.github.io/resume-vue/"
-          target="_blank"
-          rel="noopener"
-          >resume-vue</a
-        >
-        to https://tampert.github.io/resume-vue/
+          <div class="">
+            <v-sheet width="100%">
+              <v-list density="compact">
+                <v-list-item>
+                  <a
+                    href="https://tampert.github.io/resume-react/"
+                    target="_blank"
+                    rel="noopener"
+                    >resume-react
+                  </a>
+                  to https://tampert.github.io/resume-react/
+                </v-list-item>
+                <v-list-item>
+                  <a
+                    href="https://tampert.github.io/resume-angular/"
+                    target="_blank"
+                    rel="noopener"
+                    >resume-angular
+                  </a>
+                  to https://tampert.github.io/resume-angular/
+                </v-list-item>
+                <v-list-item>
+                  <a
+                    href="https://tampert.github.io/resume-vue/"
+                    target="_blank"
+                    rel="noopener"
+                    >resume-vue
+                  </a>
+                  to https://tampert.github.io/resume-vue/
+                </v-list-item>
+              </v-list>
+            </v-sheet>
+          </div>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
